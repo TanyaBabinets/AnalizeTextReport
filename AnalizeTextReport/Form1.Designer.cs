e4f4e0674f25c2f5a384d6a43dbe476f81e8a2c5
Tanyababinets <tanya060616@gmail.com>
2023-09-12 20:03:36 +0300

    Add project files.

---
+++ AnalizeTextReport/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace AnalizeTextReport
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(292, 159);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "TEXT REPORT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 30);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(756, 114);
+            this.textBox1.TabIndex = 1;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(49, 159);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(177, 24);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.Text = "SHOW on SCREEN";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(49, 195);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(135, 24);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.Text = "SAVE to FILE";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(22, 237);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(756, 126);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(526, 159);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 60);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "STOP ANALIZE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(675, 159);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(103, 60);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "RE-START";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Количество предложений",
+            "Количество символов",
+            "Количество слов"});
+            this.comboBox1.Location = new System.Drawing.Point(475, 385);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(303, 28);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(288, 388);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Выборочный поиск";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ AnalizeTextReport/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+//1.Создайте оконное приложение, использующее механизм задач (класс Task). Пользователь вводит в текстовое
+//поле некоторый текст. По нажатию на кнопку приложение должно проанализировать текст и вывести отчет. Отчёт
+//содержит информацию о:
+//■■ Количестве предложений;
+//■■ Количестве символов;
+//■■ Количестве слов;
+//■■ Количестве вопросительных предложений;
+//■■ Количестве восклицательных предложений.
+//Отчёт в зависимости от выбора пользователя отображается на экране или сохраняется в файл.
+//Задание 2
+//Добавьте к первому заданию возможность остановки и повторного запуска анализа текста
+//Задание 3
+//Добавьте к первому заданию возможность выбора типа информации, помещаемой в отчёт. Например,
+//пользователь может выбрать, что его интересует только количество предложений и слов.
+namespace AnalizeTextReport
+{
+    public partial class Form1 : Form
+    {
+        public CancellationTokenSource cancel = new CancellationTokenSource();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e) //REPORT
+        {
+            string inputText = textBox1.Text;           
+            var report = await Task.Run(() => AnalizeText(inputText));
+            if (radioButton1.Checked)// показать отчет на экране
+            {
+                textBox2.Text = report;
+            }
+            else if (radioButton2.Checked)//сохранить в файл
+            {             
+                try
+                {
+                    FileStream file = new FileStream("AnalizeText.txt", FileMode.Create, FileAccess.Write);
+                    StreamWriter writer = new StreamWriter(file);
+                    writer.WriteLine(report);
+                    MessageBox.Show("Отчет сохранен в файл.");
+                    writer.Close();
+                    file.Close();
+                   
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Ошибка чтения файла: {ex.Message}");
+                }
+                
+            }
+        }
+        private int CountSentence(string[] strings)
+        {
+            int count = 0;
+            if (strings.Length != 1)
+            {
+                int i = 0;
+                foreach (var str in strings)
+                {
+                    if (i == strings.Length - 1)
+                        break;
+                    if (str != string.Empty)
+                        count++;
+                    i++;
+                }
+            }
+            return count;
+        }
+
+        public string AnalizeText(string text)
+        {
+            int sentenceCount = CountSentence(text.Split(new char[] { '.', '!', '?' }));
+            int characterCount = text.Length;
+            int wordCount = text.Split(new char[] { ' ', ':', ';', ',', '!','?','-','.'}, StringSplitOptions.RemoveEmptyEntries).Length;
+            int questionCount = CountSentence(text.Split(new char[] { '?' }));
+            int exclamationCount = CountSentence(text.Split(new char[] { '!' }));
+            StringBuilder report = new StringBuilder();
+            report.AppendLine($"Количество предложений: {sentenceCount}");
+            report.AppendLine($"Количество символов: {characterCount}");
+            report.AppendLine($"Количество слов: {wordCount}");
+            report.AppendLine($"Количество вопросительных предложений: {questionCount}");
+            report.AppendLine($"Количество восклицательных предложений: {exclamationCount}");
+
+            return report.ToString();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e) /// show on screen
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)////RE-START
+        {
+            cancel.Cancel();
+            MessageBox.Show("Re-START");
+
+        }
+        //        Добавьте к первому заданию возможность выбора типа информации, помещаемой в отчёт.Например,
+        //пользователь может выбрать, что его интересует только количество предложений и слов.
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    AnalizeText(textBox1.Text);
+
+                    int sentenceCount = textBox1.Text.Split(new char[] { '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries).Length;
+                    StringBuilder report = new StringBuilder();
+                    report.AppendLine($"Количество предложений: {sentenceCount}");
+
+                    FileStream file = new FileStream("Analize1.txt", FileMode.Create, FileAccess.Write);
+                    StreamWriter writer = new StreamWriter(file);
+                    writer.WriteLine(report);
+                    MessageBox.Show("Кол-во предложений сохранено в файл.");
+                    writer.Close();
+                    file.Close();
+                    break;
+
+                case 1:
+                    AnalizeText(textBox1.Text);
+
+                    int characterCount = textBox1.Text.Length;
+                    StringBuilder report1 = new StringBuilder();
+                    report1.AppendLine($"Количество символов: {characterCount}");
+
+                    FileStream file2 = new FileStream("Analize2.txt", FileMode.Create, FileAccess.Write);
+                    StreamWriter writer2 = new StreamWriter(file2);
+                    writer2.WriteLine(report1);
+                    MessageBox.Show("Кол-во символов сохранено в файл.");
+                    writer2.Close();
+                    file2.Close();
+                    break;
+
+                case 2:
+                    AnalizeText(textBox1.Text);
+
+                    int wordCount = textBox1.Text.Length;
+                    StringBuilder report2 = new StringBuilder();
+                    report2.AppendLine($"Количество символов: {wordCount}");
+
+                    FileStream file3 = new FileStream("Analize3.txt", FileMode.Create, FileAccess.Write);
+                    StreamWriter writer3 = new StreamWriter(file3);
+                    writer3.WriteLine(report2);
+                    MessageBox.Show("Кол-во слов сохранено в файл.");
+                    writer3.Close();
+                    file3.Close();
+                    break;
+            }
+        }
+
+        private async void button2_Click(object sender, EventArgs e)//STOP
+        {
+            cancel = new CancellationTokenSource();
+            try
+            {
+                await Task.Run(() => AnalizeText(cancel.Token.ToString()), cancel.Token);
+                MessageBox.Show("STOP!");
+            }
+            catch (OperationCanceledException)
+            {
+
+                MessageBox.Show("Задача отменена.");
+            }
+        }
+    }
+}
